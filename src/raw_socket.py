1f46d4060b14e4e555e9d6df48d0c55ee0bcdf88
Dylan Cabrera <dylancabrera024@gmail.com>
2025-09-28 00:56:52 -0400

    Add link layer socket manager

---
+++ src/raw_socket.py
@@ -0,0 +1,38 @@
+import socket
+
+class RawSocketHandler:
+    def __init__(self , interface = 'ens33'):
+        self.interface = interface
+        self.socket = None
+        self.mac_address = self.get_mac_address()
+    def get_mac_address(self):
+        try:
+            with open(f'/sys/class/net/{self.interface}/address','r') as f:
+                return f.read().strip()
+        except:
+            return "00:00:00:00:00:00"
+    def create_socket(self):
+        try:
+            self.socket = socket.socket(socket.AF_PACKET,socket.SOCK_RAW,socket.htons(0x0003))
+            self.socket.bind((self.interface,0))
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+    def send_frame(self,frame):
+        if self.socket:
+            try:
+                return self.socket.send(frame)
+            except Exception as e:
+                print(f"Error enviando: {e}")
+                return 0
+        return 0
+    def receive_frame(self):
+        if self.socket:
+            try:
+                return self.socket.recvfrom(65536)
+            except Exception as e:
+                return None , None
+            return None , None
+a = RawSocketHandler()
+a.get_mac_address()

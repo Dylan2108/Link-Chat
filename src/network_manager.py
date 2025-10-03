ebe6e80a6e224899c70d17d665124762e962ac8a
Dylan Cabrera <dylancabrera024@gmail.com>
2025-10-03 07:54:36 -0400

    Add network manager core

---
+++ src/network_manager.py
@@ -0,0 +1,65 @@
+import threading
+from raw_socket import RawSocketHandler
+from protocol import Protocol , MessageType
+from frame_builder import FrameBuilder
+from discovery import NetworkDiscovery
+
+class NetworkManager:
+    def __init__(self):
+        self.socket_manager = RawSocketHandler()
+        self.frame_builder = FrameBuilder()
+        self.discovery = NetworkDiscovery(self.socket_manager,self.frame_builder)
+        self.running = False
+        self.message_callbacks = []
+
+    def start(self):
+        #Iniciar servicios de red
+        if not self.socket_manager.create_socket():
+            return False
+        self.running = True
+
+        #Hilo de recepcion
+        self.receive_thread = threading.Thread(target=self.receive_loop)
+        self.receive_thread.daemon = True
+        self.receive_thread.start()
+    
+    def receive_loop(self):
+        #Bucle de recepcion
+        while self.running:
+            frame , addr = self.socket_manager.receive_frame()
+            if frame:
+                self.process_frame(frame)
+            
+    def _process_frame(self,frame):
+        #Procesar Frame
+        parsed_frame = self.frame_builder.parse_ethernet_frame(frame)
+        if not parsed_frame or parsed_frame['eth_type'] != Protocol.ETH_TYPE:
+            return
+        
+        parsed_message = Protocol.parse_message(parsed_frame['payload'])
+        if not parsed_message:
+            return
+        
+        #Notificar a callbacks registrados
+        for callback in self.message_callbacks:
+            callback(parsed_frame['src_mac'],parsed_message['type'],parsed_message['data'])
+    
+    def send_message(self,dest_mac,message_type,data):
+        #Enviar mensaje
+        payload = Protocol.create_message(message_type,data)
+        frame = self.frame_builder.build_ethernet_frame(
+            dest_mac,
+            self.socket_manager.mac_address,
+            Protocol.ETH_TYPE,
+            payload
+        )
+        return self.socket_manager.send_frame(frame)
+    
+    def register_callback(self,callback):
+        #Registrar callback para mensajes recibidos
+        self.message_callbacks.append(callback)
+    
+    def stop(self):
+        #Detener servicios
+        self.running = False
+        self.socket_manager.close()
